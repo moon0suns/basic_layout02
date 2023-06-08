6691a03fa8b4d6adaa1d54fd67847d62ee2f4931
moon0suns <133613683+moon0suns@users.noreply.github.com>
2023-06-08 14:48:53 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,13 @@
+//html 읽고나서 mainjs에서 실행해라
+$(function () {
+
+    $('.main_slide').slick({
+        vertical: true,
+        autoplay: true,
+        autoplaySpeed: 5000,
+        arrows: false,
+        dots: true,
+
+    })
+
+})
